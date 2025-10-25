34826bb3e6ed1ab8af851541e421f03495edee2a
Hashim Ali Munir <hashimalimunir@gmail.com>
2025-10-25 14:15:12 +0500

    Update BugsPort binary target URL in Package.swift to point to the correct GitHub release for version 0.1.1-alpha-test

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "BugsPort",
-            url: "https://github.com/hash-line/bugsport-client/releases/download/v0.1.1-pre-alpha/shared.xcframework.zip",
+            url: "https://github.com/hash-line/bugsport-ios/releases/download/v0.1.1-alpha-test/shared.xcframework.zip",
             checksum: "156829e89e87e885e5530fa41118616b73508b177057fe7a28421396d7b530f4"
         )
     ]
